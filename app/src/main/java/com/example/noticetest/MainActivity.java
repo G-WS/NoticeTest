01898268c45190bf4d7b63c4e3e721230dbdbe96
G-WS <3355139577@qq.com>
2022-08-17 12:34:12 +0800

    0817

--- app/src/main/java/com/example/noticetest/MainActivity.java
+++ app/src/main/java/com/example/noticetest/MainActivity.java
@@ -14,51 +14,61 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private NotificationManager mNotificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button sendNotice = (Button) findViewById(R.id.send_notice);
-        sendNotice.setOnClickListener(MainActivity.this);
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        /**
+         + 1、 构建通知渠道，传入三个参数
+         + param channelId      渠道 Id，全局唯一即可
+         + param channelName    渠道名称，显示给用户，需要清楚表达渠道的用途
+         + param importance     通知的重要等级，不同的通知等级决定通知的不同行为，
+         + 从高到底主要有 IMPORTANCE_HIGH、IMPORTANCE_DEFAULT、IMPORTANCE_LOW、IMPORTANCE_MIN这几种
+         + 当然这里只是初始状态下的重要等级，用户可以随时手动更改某个通知渠道的重要等级，开发者是无法干预的。
+         */
+        //通知渠道的name，自己设定
+        String name = getString(R.string.channel_name);
+        //通知渠道的id
+        String id = "my_channel_01";
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(id, name, NotificationManager.IMPORTANCE_DEFAULT);
+            mNotificationManager.createNotificationChannel(notificationChannel);
+            Button sendNotice = (Button) findViewById(R.id.send_notice);
+            sendNotice.setOnClickListener(MainActivity.this);
+        }
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.send_notice:
-                //创建NotificationManager对通知进行管理
-                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-                //创建渠道并设置器重要性
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-                    //通知渠道的name，自己设定
-                    String name = getString(R.string.channel_name);
-                    //通知渠道的id
-                    String id = "my_channel_01";
-                    NotificationChannel channel = new NotificationChannel(name,id,
-                            NotificationManager.IMPORTANCE_DEFAULT);
-                    manager.createNotificationChannel(channel);
-                    //设置点击事件
-//                    Intent intent = new Intent(MainActivity.this,MainActivity.class);
-//                    PendingIntent pi = PendingIntent.getActivity(MainActivity.this,0,intent,0);
-//                    //创建通知内容，ID必须在实例化Notification的时候对应上，否则通知栏无效，系统则会将Toast放入一个错误
-                    Notification notification = new NotificationCompat.Builder(MainActivity.this,"my_channel_01")
-                            .setContentTitle("My notification")             //设置标题
-                            .setContentText("Hello World!")                 //设置正文
-                            .setWhen(System.currentTimeMillis())          //设置被创建的时间
-                            .setSmallIcon(R.mipmap.ic_launcher)             //设置通知的小图标
-                            .setAutoCancel(true)
-//                            .setContentIntent(pi)//点击时通知取消
-                            .setLargeIcon(BitmapFactory.decodeResource(getResources(),
-                                    R.mipmap.ic_launcher))    //设置通知的大图标
-                            .build();
+        //第二次尝试
+        //创建通知渠道
+        // Context.getSystemService()，该方法接收一个字符串参数用于确定获取系统的哪个服务。
 
-                    manager.notify(1,notification);
+        /**
+         * 调用 AndroidX 中的兼容 API 方法，NotificationCompat.Builder()
+         * param  context
+         * param  channelId   渠道Id，需要和我们创建的渠道 Id 相匹配
+         */
+        //                    Notification notification =
+        //                            new NotificationCompat.Builder(context, channelId);
+        // 上述对象在调用最终的 build() 方法之前可以连缀任意多的设置方法来创建一个丰富的 Notification 对象
 
-
-                }
-
-        }
+        //通知渠道的id
+        String id = "my_channel_01";
+        Notification notification = new NotificationCompat.Builder(MainActivity.this, id)
+                // 指定通知的标题内容，下拉状态栏显示
+                .setContentTitle("This is content title")
+                // 指定通知的正文内容，下拉状态栏显示
+                .setContentText("This is content text")
+                // 设置通知的小图标，显示在状态栏上，只能使用纯 alpha 图层的图片
+                .setSmallIcon(R.drawable.apple)
+                // 设置通知的大图标，下拉状态栏显示
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.apple_large))
+                .build();
+        mNotificationManager.notify(1, notification);
     }
 }
